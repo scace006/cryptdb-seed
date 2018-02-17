07a4090365dae823490f803936356af2ebc84adb
jonycodes <mr.joannier@yahoo.com>
2018-02-16 19:20:44 -0500

    fixing script

--- scripts/install.rb
+++ scripts/install.rb
@@ -37,7 +37,7 @@ def get_pkgs
                 libevent-dev libglib2.0-dev libgmp-dev                          \
                 mysql-server libaio-dev automake                                \
                 gtk-doc-tools flex cmake libncurses5-dev                        \
-                g++ make cscope;                                         
+                g++ make cscope                                      
     })
 end
 
