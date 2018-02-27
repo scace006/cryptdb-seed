d9b9f90f348a964393ef1c1020e2cd70271e53a1
jonycodes <mr.joannier@yahoo.com>
2018-02-26 22:31:52 -0500

    changes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,13 @@
 # install python deps
 apt-get install -y python-pip python-pandas python-numpy \
 && pip install --upgrade pip \
-&& pip install -r ../research/data/requirements.txt
+&& pip install -r /opt/cryptdb/data/requirements.txt
 
 # install web onion tool
 apt-get install -y apache2 \
         apache2-utils apache2-bin \
         libapache2-mod-php5 \
         php5-cli php5-common php5-mysql php5 \
-        && cd ../tools/php && ./install.sh \
+        && /copt/cryptdb/tools/php/install.sh \
         && service apache2 restart \
-        && cd ../..
+        && cd $(EDBDIR)
